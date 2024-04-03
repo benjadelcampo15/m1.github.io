71b58d03bb17231978ae8479ed882add4e80a8fc
benjadelcampo15 <benjadelcampo15@gmail.com>
2024-04-03 13:57:40 -0300

    foto html arreglada

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,14 @@
-
-
 class Activity {
-    constructor( id, title , description , url){
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.url = url;
+    constructor(id , title , url , description){
+         this.id = id,
+         this.title = title,
+         this.url = url,
+         this.description = description
+
     }
 }
 
-class Repository {
-
+class Repository{
     constructor(){
 
         this.activities = [];
@@ -18,139 +16,131 @@ class Repository {
         this.id = 0;
 
     }
-    
-    createActivitie(title , description , url) {
-        
-        const id = this.id++;
 
-        const actividad = new Activity( id ,title , description , url);
-    
-        this.activities.push(actividad);
+    createActivitie(title , url , descripcion){
+
+        const id = this.id ++;
+
+        const activity = new Activity(id , title , url , descripcion);
+
+        this.activities.push(activity);
 
     }
 
-    getAllActivities() {
-
+    getAllActivities(){
         return this.activities;
-
     }
 
-     deleteActivitie(id){
-
-         this.activities =  this.activities.filter(Activity => Activity.id !== id);
-
-     }
+    deleteActivities(id){
+        this.activities = this.activities.filter(Activity => Activity.id != id);
+        this.getAllActivities();
+    }
 }
 
-const repo = new Repository();
+const repository = new Repository();
 
-hacerTarjeta = (actividad) =>{
-     
-    const {id ,title , description , url} = actividad;
+CreateCard = (activity) => {
 
-    const h3 =  document.createElement('h3');
+   const  {id ,title , url , description} = activity;
 
-    const p = document.createElement('p');
+   const tituloElemento = document.createElement('h2');
 
-    const img = document.createElement('img');
+   const urlElemento = document.createElement('img');
 
-    const eliminar = document.createElement("img");
+   const pElemento = document.createElement('p');
 
-    h3.innerHTML= title;
+   const eliminar = document.createElement('img');
 
-    p.innerHTML = description;
+   eliminar.src = 'imagenes/borrar.png';
+
+   tituloElemento.innerHTML = title;
+
+   urlElemento.src = url;
+
+   pElemento.innerHTML = description;
+
+   eliminar.classList.add('cardEliminar');
+
+   tituloElemento.classList.add('cardTitle');
+
+   urlElemento.classList.add('cardUrl');
+
+   pElemento.classList.add('cardDescription');
+
+   eliminar.addEventListener('click' , borrar = (event) => {
+
+    div.remove();
+
+    repository.deleteActivities(id);
+
+   })
+
+   const div = document.createElement('div');
+
+   div.classList.add('cardsActivities');
+
+   div.appendChild(tituloElemento);
+
+   div.appendChild(urlElemento);
+
+   div.appendChild(pElemento);
+
+   div.appendChild(eliminar);
+
+   return div;
+}
+
+Apender = () => {
+    const contenedor = document.getElementById('contenedor');
+    const actividades = document.getElementById('titulo');
+    const h1 = document.createElement('h1');
+    contenedor.innerHTML = ('');
+    actividades.innerHTML = '';
+    h1.innerHTML = 'Activities';
+    actividades.appendChild(h1);
+    const array = repository.getAllActivities().map(Activity => CreateCard(Activity));
     
-    img.src = url;
-
-    // eliminar.innerHTML = "X"
-
-    eliminar.src = "imagenes/cruz.png"
-
-    eliminar.classList.add('boton');
-
-    h3.classList.add('tarjetaTitulo');
-
-    p.classList.add('tarjetaDescripcion');
-
-    img.classList.add('tarjetaImg');
-
-    const div = document.createElement('div');
-
-    div.appendChild(h3);
-
-    div.appendChild(p);
-
-    div.appendChild(img);
-
-    div.appendChild(eliminar);
-
-    div.classList.add("tarjetaActividad");
-
-    eliminar.addEventListener("click" , (event) => {
-         
-        repo.deleteActivitie(id)
-
-        div.remove()
-
+    array.forEach(Activity => {
+        contenedor.appendChild(Activity);
     })
-
-    return div;
-
 }
 
-appenderDeTarjetas = () => {
 
-    const section = document.getElementById('tarjetas2');
 
-    section.innerHTML = '';
+const Handler = () => {
 
-    const actividades = repo.getAllActivities();
+    const form = document.getElementById('formulario');
 
-    const array =  actividades.map(actividad => hacerTarjeta(actividad))
+    
+    const InputTitle = document.getElementById('title').value;
 
-        array.forEach(actividad => {
+    const InputUrl = document.getElementById('url').value;
+    
+    const InputDescription = document.getElementById('description').value;
 
-        section.appendChild(actividad)
-        
-     }); 
-
-     
-}
-
-const handler = () => {
-
-    const titulo = document.getElementById('titulo').value;
-
-    const descripcion = document.getElementById('descripcion').value;
-
-    const url = document.getElementById('url').value;
-
-    if (titulo  == '') {
+    if (InputTitle  == '') {
 
         alert('Campo incompleto : titulo');
         
-    }else if  (descripcion == '') {
+    }else if  (InputDescription == '') {
 
         alert('Campo incompleto : descripcion')
         
-    }else if (url =='') {
+    }else if (InputUrl =='') {
 
         alert('Campo incompleto: url de imagen')
 
     }else {
-        
-        repo.createActivitie(titulo , descripcion , url);
+      
+        repository.createActivitie(InputTitle , InputUrl , InputDescription);
 
-        appenderDeTarjetas();
-
+        Apender();
+    
+        form.reset();
     }
 }
-const boton = document.getElementById('boton');
 
-boton.addEventListener('click' , handler);
+const button = document.getElementById('button');
 
-
-
-
-
+button.addEventListener('click' , Handler)
 
